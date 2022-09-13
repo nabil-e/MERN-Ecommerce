73d05410f7d733abdf425f95254b2406d0e87319
nabil el assal <nabilelassal@gmail.com>
2022-09-13 20:57:55 +0200

    add react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './pages/Home'
+import Product from './pages/Product'
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <Home />
+          <Routes>
+            <Route path='/' element={< Home/>} exact />
+            <Route path='/product/:id' element={< Product/>} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
