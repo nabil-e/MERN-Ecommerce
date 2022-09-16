16aca22c61ce120c9bafdd103554416c1a2f4e48
nabil el assal <nabilelassal@gmail.com>
2022-09-16 15:39:26 +0200

    add some Custom Error Handler

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDb from './config/db.js'
 import productRoutes from './routes/productRoute.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 const app = express()
 dotenv.config()
@@ -13,6 +14,10 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
