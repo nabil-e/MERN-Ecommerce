34f8609f65aba7ed2b4ef3f676b431781c78f44d
nabil el assal <nabilelassal@gmail.com>
2022-09-17 20:25:11 +0200

    add Cart Reducer & Add To Cart Action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { productListReducer, productDetailsReducer } from './reducers/productReducer'
+import { cartReducer } from './reducers/cartReducer'
 
-//const reducer = combineReducers({})
+const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
 
+const initialState = {
+    cart: {
+        cartItems: cartItemsFromStorage
+    }
+}
 const store = configureStore({
     reducer: {
         productList: productListReducer,
-        productDetails: productDetailsReducer
-    }
+        productDetails: productDetailsReducer,
+        cart: cartReducer,
+    },
+    preloadedState: initialState
 })
 
 export default store
