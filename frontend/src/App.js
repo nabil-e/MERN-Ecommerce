6e0fccbff637c71f02f9533b475422fc075949f4
nabil el assal <nabilelassal@gmail.com>
2022-09-17 17:30:39 +0200

    add Qty Select, add To Cart Button and cart page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './pages/Home'
 import Product from './pages/Product'
+import Cart from './pages/Cart'
 
 const App = () => {
   return (
@@ -12,8 +13,10 @@ const App = () => {
       <main className='py-3'>
         <Container>
           <Routes>
-            <Route path='/' element={< Home/>} exact />
-            <Route path='/product/:id' element={< Product/>} />
+            <Route path='/' element={<Home />} exact />
+            <Route path='/product/:id' element={<Product />} />
+            <Route path='/cart/:id' element={<Cart />} />
+            <Route path='/cart/' element={<Cart />} />
           </Routes>
         </Container>
       </main>
