ec36186a60cb287aab5f52d2103b3ce035587c5b
nabil el assal <nabilelassal@gmail.com>
2022-09-27 16:47:47 +0200

    Add controller and modify route

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,22 +1,16 @@
 import express from "express"
-import asyncHandler from 'express-async-handler'
-import Product from '../models/productModel.js'
+import { getProductByid, getProducts } from "../controllers/productController.js"
 
 const router = express.Router()
 
-router.get('/', asyncHandler(async(req, res) => {
-    const products = await Product.find({})
-    res.json(products)
-}))
+// @desc  Fetch all products
+// @route GET /api/products
+// access Public
+router.route('/').get(getProducts)
 
-router.get('/:id', asyncHandler(async(req, res) => {
-    const product = await Product.findById(req.params.id)
-    if(product){
-        res.json(product)
-    }else {
-        res.status(404)
-        throw new Error('Product not found')
-    }
-}))
+// @desc  Fetch single product
+// @route GET /api/products/:id
+// access Public
+router.route('/:id').get(getProductByid)
 
 export default router
